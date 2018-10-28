9534feeb1e0ab06d16e5b2261af271a446d65608
scottjbos <s.bos@kunzleigh.com>
2018-10-27 23:05:06 -0400

    setting only json a valid format

--- AspnetTodoApi/TodoApi/App_Start/WebApiConfig.cs
+++ AspnetTodoApi/TodoApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace TodoApi
@@ -10,6 +8,8 @@ namespace TodoApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
